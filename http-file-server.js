6b89b5feffdba7c728419d48e19f8c2e27dc28f1
TCBarrett <tcbarrett@gmail.com>
2018-08-16 21:23:26 +0100

    Complete http file server

--- http-file-server.js
+++ http-file-server.js
@@ -1,17 +1,27 @@
 var http = require( 'http' )
+var fs   = require( 'fs' )
 // var bl   = require( 'bl' )
 
 let port = process.argv[2]
+let file = process.argv[3]
 
 let server = http.createServer( function(request, response ){
-    let hwtxt = 'HTTP Hello World!'
     response.writeHead( 200,{
-        'Content-Length': hwtxt.length,
         'Content-Type':'text/plain'
     })
-    console.log('header sent')
-    response.end( hwtxt )
-    console.log( 'request conplete')
+
+    content = fs.createReadStream( file )
+    content.on('readable', () => {
+        response.end( content.read() )
+    })
 })
 
 server.listen(port)
+
+// var server = http.createServer(function (req, res) {
+//     res.writeHead(200, { 'content-type': 'text/plain' })
+
+//     fs.createReadStream(process.argv[3]).pipe(res)
+// })
+
+// server.listen(Number(process.argv[2]))
